2ff2ff271cf0ba2460735790f62dc90a4c3e118d
Ayumi Manome <ayumimanome@Ayumi-Fujisaki-Manomes-MacBook-Pro.local>
2025-09-22 09:31:04 -0400

    added erie_airtemp_bar_and_timeseries.py

--- ice_code/erie_airtemp_bar_and_timeseries.py
+++ ice_code/erie_airtemp_bar_and_timeseries.py
@@ -11,32 +11,32 @@ nlim=10
 start_date = "1897-01-01"
 end_date = "2023-10-22"
 
-df_tol = pd.read_csv("./data/toledo_all.csv",usecols=['DATE','TAVE'])
+df_tol = pd.read_csv("../ice_data/air_temp_data/toledo_all.csv",usecols=['DATE','TAVE'])
 df_tol['DATE']=pd.to_datetime(df_tol['DATE'],format='%Y-%m-%d')
 df_tol=df_tol.set_index('DATE')
 df_tol=df_tol[start_date:end_date].interpolate(limit=nlim)
 print(df_tol[df_tol.index.duplicated()])
 
 
-df_eri = pd.read_csv("./data/erie_all.csv",usecols=['DATE','TAVE'])
+df_eri = pd.read_csv("../ice_data/air_temp_data/erie_all.csv",usecols=['DATE','TAVE'])
 df_eri['DATE']=pd.to_datetime(df_eri['DATE'],format='%Y-%m-%d')
 df_eri=df_eri.set_index('DATE')
 df_eri=df_eri[start_date:end_date].interpolate(limit=nlim)
 print(df_eri[df_eri.index.duplicated()])
 
-df_det = pd.read_csv("./data/detroit_all.csv",usecols=['DATE','TAVE'])
+df_det = pd.read_csv("../ice_data/air_temp_data/detroit_all.csv",usecols=['DATE','TAVE'])
 df_det['DATE']=pd.to_datetime(df_det['DATE'],format='%Y-%m-%d')
 df_det=df_det.set_index('DATE')
 df_det=df_det[start_date:end_date].interpolate(limit=nlim)
 print(df_det[df_det.index.duplicated()])
 
-df_clv = pd.read_csv("./data/cleveland_all.csv",usecols=['DATE','TAVE'])
+df_clv = pd.read_csv("../ice_data/air_temp_data/cleveland_all.csv",usecols=['DATE','TAVE'])
 df_clv['DATE']=pd.to_datetime(df_clv['DATE'],format='%Y-%m-%d')
 df_clv=df_clv.set_index('DATE')
 df_clv=df_clv[start_date:end_date].interpolate(limit=nlim)
 print(df_clv[df_clv.index.duplicated()])
 
-df_buf = pd.read_csv("./data/buffalo_all.csv",usecols=['DATE','TAVE'])
+df_buf = pd.read_csv("../ice_data/air_temp_data/buffalo_all.csv",usecols=['DATE','TAVE'])
 df_buf['DATE']=pd.to_datetime(df_buf['DATE'],format='%Y-%m-%d')
 df_buf=df_buf.set_index('DATE')
 df_buf=df_buf[start_date:end_date].interpolate(limit=nlim)
@@ -51,11 +51,12 @@ df_ave['buf'] = df_buf['TAVE']
 df_ave['all_ave'] = df_ave[["eri","det","clv","buf","tol"]].mean(axis=1)
 
 
+
 # save to a csv file
 #df_ave['all_ave'].to_csv("Lake_Erie_Average.csv")
 
 
-print(df_ave.head())
+#print(df_ave.head())
 
 fig, ax = plt.subplots(3, sharex=False,figsize=(15,7))
 
@@ -85,23 +86,23 @@ ax[1].text(df_tol.index[25],df_tol['TAVE'].rolling(mper,center=True,min_periods=
 
 
 # plot all lakes
-dfs = pd.read_csv("./data/Lake_Superior_Average.csv")
+dfs = pd.read_csv("../ice_data/air_temp_data/Lake_Superior_Average.csv")
 dfs['DATE']=pd.to_datetime(dfs['DATE'],format='%Y-%m-%d')
 dfs=dfs.set_index('DATE')
 
-dfh = pd.read_csv("./data/Lake_Huron_Average.csv")
+dfh = pd.read_csv("../ice_data/air_temp_data/Lake_Huron_Average.csv")
 dfh['DATE']=pd.to_datetime(dfh['DATE'],format='%Y-%m-%d')
 dfh=dfh.set_index('DATE')
 
-dfm = pd.read_csv("./data/Lake_Michigan_Average.csv")
+dfm = pd.read_csv("../ice_data/air_temp_data/Lake_Michigan_Average.csv")
 dfm['DATE']=pd.to_datetime(dfm['DATE'],format='%Y-%m-%d')
 dfm=dfm.set_index('DATE')
 
-dfe = pd.read_csv("./data/Lake_Erie_Average.csv")
+dfe = pd.read_csv("../ice_data/air_temp_data/Lake_Erie_Average.csv")
 dfe['DATE']=pd.to_datetime(dfe['DATE'],format='%Y-%m-%d')
 dfe=dfe.set_index('DATE')
 
-dfo = pd.read_csv("./data/Lake_Ontario_Average.csv")
+dfo = pd.read_csv("../ice_data/air_temp_data/Lake_Ontario_Average.csv")
 dfo['DATE']=pd.to_datetime(dfo['DATE'],format='%Y-%m-%d')
 dfo=dfo.set_index('DATE')
 
